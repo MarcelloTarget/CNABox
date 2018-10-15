fe4f2f55d8c7c1292337ddf5132727c20d90b6b5
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-15 14:03:30 -0300

    comitts

---
+++ src/test/java/tests/MenuFinanceiro/MenuFinanceiroAnecipacaoRecebivelTest.java
@@ -0,0 +1,18 @@
+package tests.MenuFinanceiro;
+import org.junit.Test;
+import Page.MenuFinanceiro.MenuFinanceiroAntecipacaodeRecebivel;
+import qa.cnabox.core.BaseTest;
+
+public class MenuFinanceiroAnecipacaoRecebivelTest  extends BaseTest {
+
+	MenuFinanceiroAntecipacaodeRecebivel page = new MenuFinanceiroAntecipacaodeRecebivel();
+	@Test
+	
+	public void MenuFinanceiroAntecipacaodeRecebivel() throws InterruptedException { 
+
+		
+	
+		
+		
+	}
+}
