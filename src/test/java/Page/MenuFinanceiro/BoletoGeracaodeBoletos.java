401f82362275517b58b3879cd23322af06fcd527
carolinasampognaTgT <41452195+carolinasampognaTgT@users.noreply.github.com>
2018-10-18 11:16:27 -0300

    atualização

---
+++ src/test/java/Page/MenuFinanceiro/BoletoGeracaodeBoletos.java
@@ -0,0 +1,19 @@
+package Page.MenuFinanceiro;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import qa.cnabox.core.BasePage;
+
+public class BoletoGeracaodeBoletos  extends BasePage {
+	
+	
+
+}
